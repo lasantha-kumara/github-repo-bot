85ce9da3feae6e976fa03bbcb36d409ff697e6af
Lasantha <lasantha1130@gmail.com>
2022-09-22 11:05:42 +0530

    change paltform from ubuntu to windows

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     stealth(driver,
             languages=["en-US", "en"],
             vendor="duckduckgo.com",
-            platform="Ubuntu64",
+            platform="win64",
             webgl_vendor="Intel Inc.",
             renderer="Intel Iris OpenGL Engine",
             fix_hairline=True,
