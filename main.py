490e8358093b56ceb21b6ef2c6dff877ad4be920
Lasantha <lasantha1130@gmail.com>
2022-09-22 11:38:35 +0530

    add exeption if user didn't enter a repo name

--- main.py
+++ main.py
@@ -14,33 +14,37 @@ PASSWORD = os.getenv("GITHUB_PASSWORD")
 
 
 def main():
-    repo_name = sys.argv[1]
+    try:
+        repo_name = sys.argv[1]
 
-    driver = uc.Chrome()
-    driver.maximize_window()
+        driver = uc.Chrome()
+        # driver.maximize_window()
 
-    # go to github new repo page
-    driver.get("https://github.com/new")
+        # go to github new repo page
+        driver.get("https://github.com/new")
 
-    username = driver.find_element(By.NAME, "login")
-    username.send_keys(USERNAME)
-    sleep(2)
-    password = driver.find_element(By.NAME, "password")
+        username = driver.find_element(By.NAME, "login")
+        username.send_keys(USERNAME)
+        sleep(2)
+        password = driver.find_element(By.NAME, "password")
 
-    # press enter after entering password
-    password.send_keys(PASSWORD + Keys.ENTER)
-    sleep(2)
+        # press enter after entering password
+        password.send_keys(PASSWORD + Keys.ENTER)
+        sleep(2)
 
-    repo_name_input_box = driver.find_element(By.ID, "repository_name")
-    repo_name_input_box.send_keys(repo_name)
-    sleep(2)
+        repo_name_input_box = driver.find_element(By.ID, "repository_name")
+        repo_name_input_box.send_keys(repo_name)
+        sleep(2)
 
-    create_repo_btn = driver.find_element(
-        By.XPATH, '//*[@id="new_repository"]/div[5]/button')
-    create_repo_btn.click()
+        create_repo_btn = driver.find_element(
+            By.XPATH, '//*[@id="new_repository"]/div[5]/button')
+        create_repo_btn.click()
 
-    # wait for an hour before closing the window
-    sleep(20)
+        # wait for an 20 seconds before closing the window
+        sleep(20)
+
+    except IndexError:
+        print("Enter a repo name")
 
 
 main()
